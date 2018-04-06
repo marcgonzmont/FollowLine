a9dbb2006d88e8014a6ad15cbe4d64e755f1a486
Unknown <marcgonzmont@gmail.com>
2018-04-06 09:07:31 +0200

    Update ref and curr points

--- MyAlgorithm.py
+++ MyAlgorithm.py
@@ -5,6 +5,7 @@ import cv2
 import threading
 import time
 from datetime import datetime
+from scipy.spatial import distance as dist
 
 time_cycle = 80
 
@@ -16,6 +17,7 @@ class MyAlgorithm(threading.Thread):
         self.motors = motors
         self.imageRight=None
         self.imageLeft=None
+        self.ref_points = None
         self.stop_event = threading.Event()
         self.kill_event = threading.Event()
         self.lock = threading.Lock()
@@ -45,11 +47,15 @@ class MyAlgorithm(threading.Thread):
         return tempImage
 
     def run (self):
+        first = True
+        # ref_points = None
+        # curr_points = None
 
         while (not self.kill_event.is_set()):
             start_time = datetime.now()
             if not self.stop_event.is_set():
-                self.execute()
+                self.execute(first)
+                first = False
             finish_Time = datetime.now()
             dt = finish_Time - start_time
             ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
@@ -69,15 +75,28 @@ class MyAlgorithm(threading.Thread):
     def kill (self):
         self.kill_event.set()
 
-    def execute(self):
+    def execute(self, first):
         #GETTING THE IMAGES
         imageLeft = self.cameraL.getImage().data
-        # imageRight = self.cameraR.getImage().data
+        divisions = 5
+        # List of middle points of the road
+        curr_points = None
+        # Compute the frame and get the middle points of the road
+        imgLeft, img_L_filtered, points = self.getPoints(imageLeft, divisions)
+        # if len(points) <= 1:
+        #     print("I CAN'T SEE THE ROAD!!")
+        # else:
+        #     print("OK!")
+        # Set the reference points
+        if first:
+            self.ref_points = points
 
-        imgLeft, img_L_filtered = getPoints(imageLeft)
+        # Other frames, get current points and compute distances with the reference points
+        else:
+            curr_points = points
 
-        # Add your code here
-        # print ("Running")
+
+        # distances = getDistances(ref_points, curr_points)
 
         #EXAMPLE OF HOW TO SEND INFORMATION TO THE ROBOT ACTUATORS
         #self.motors.sendV(10)
@@ -87,10 +106,12 @@ class MyAlgorithm(threading.Thread):
         self.setRightImageFiltered(np.uint8(img_L_filtered))
         self.setLeftImageFiltered(np.uint8(imageLeft))
 
-def getPoints(img):
+    def getPoints(self, img, divisions):
         kernel_b = (5,5)
         kernel_c = (3,3)
 
+        # lower_color = np.array([0, 100, 100])
+        # upper_color = np.array([180, 255, 255])
         lower_color = np.array([0, 100, 100])
         upper_color = np.array([180, 255, 255])
 
@@ -102,17 +123,20 @@ def getPoints(img):
 
         h, w = mask.shape[:2]
         h_m = h // 2
-        # ref_points = {}
-        # curr_points = {}
+        points = []
 
-        for y in range(h_m, h, h_m // 5):
-            cv2.line(img, (0, y), (w, y), (255, 0, 0), 1)
-            whites = np.nonzero(mask[y,:])
-            # x = whites[0]+whites[len(whites[0]) - 1]/2
-            print(x)
-            # cv2.circle(img, (int(x), int(y)), 2, (0, 255, 0), 3)
+        for y in range(h_m, h, h_m // divisions):
+            whites = np.nonzero(mask[y, :])
+            if len(whites[0]) != 0:
+                cv2.line(img, (0, y), (w, y), (255, 0, 0), 1)
+                w_tmp = (whites[0][0] + whites[0][-1]) // 2
+                cv2.circle(img, (int(w_tmp), int(y)), 2, (0, 255, 0), 3)
+                points.append([int(w_tmp), int(y)])
 
         mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
-        # test = np.hstack([img, mask])
 
-        return img, mask
+        return img, mask, points
+
+    def getDistances(self, ref, curr):
+        distances = []
+        # for
